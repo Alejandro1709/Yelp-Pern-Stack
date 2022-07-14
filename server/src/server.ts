ab4ae1f0c93f9ed1531845293d9cb2548fb116b6
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-07-13 21:51:47 -0500

    Add: dummy restaurants array and replace all routes with the array

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,26 @@
 import express, { Request, Response } from 'express';
+import Restaurant from './types/restaurant';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
+
+const restaurants: Array<Restaurant> = [
+  {
+    id: 1,
+    name: 'Mcdonalds',
+    location: 'New Yourk, USA',
+    priceRange: 4,
+  },
+  {
+    id: 2,
+    name: 'Burguer King',
+    location: 'Las Vegas, USA',
+    priceRange: 3,
+  },
+];
 
 // Middlewares
 app.use(express.json());
@@ -24,29 +40,41 @@ app.get('/api/v1/restaurants', (req: Request, res: Response) => {
   res.status(200).json({
     success: true,
     data: {
-      count: 2,
-      restaurants: ['Mcdonalds', 'Burger King'],
+      count: restaurants.length,
+      restaurants,
     },
   });
 });
 
 app.get('/api/v1/restaurants/:id', (req: Request, res: Response) => {
+  const foundRestaurant = restaurants.find((r) => r.id === +req.params.id);
+
   res.status(200).json({
     success: true,
     data: {
       count: 1,
-      restaurants: ['Mcdonalds'],
+      restaurants: foundRestaurant,
     },
   });
 });
 
 app.post('/api/v1/restaurants', (req: Request, res: Response) => {
-  console.log(req.body);
+  const { name, location, priceRange } = req.body;
+
+  const newRestaurant: Restaurant = {
+    id: restaurants.length + 1,
+    name,
+    location,
+    priceRange,
+  };
+
+  restaurants.push(newRestaurant);
+
   res.status(200).json({
     success: true,
     data: {
       count: 1,
-      restaurants: ['Mcdonalds'],
+      restaurants: newRestaurant,
     },
   });
 });
---
+++ server/src/types/restaurant.d.ts
@@ -0,0 +1,6 @@
+export default interface Restaurant {
+  id?: number;
+  name: string;
+  location: string;
+  priceRange: number;
+}
