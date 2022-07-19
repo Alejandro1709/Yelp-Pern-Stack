71da691573e87520b2bacd8f09d2193edd4f11ea
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-07-18 19:29:16 -0500

    Add: connection info

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,6 +1,12 @@
 import { Pool } from 'pg';
 
-const pool = new Pool();
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'yelp',
+  password: '',
+  port: 5432,
+});
 
 export default {
   query: (text: string, params: any) => pool.query(text, params),
