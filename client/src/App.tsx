e365b0d5503fc52405dd0e6037c11d2474f28c8b
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-07-23 13:42:45 -0500

    Add routes for viewing restaurant and edit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import RestaurantPage from './pages/RestaurantPage';
+import EditRestaurantPage from './pages/EditRestaurantPage';
 import './App.css';
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
     <div className='App'>
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='/restaurants/:id' element={<RestaurantPage />} />
+        <Route path='/restaurants/:id/edit' element={<EditRestaurantPage />} />
       </Routes>
     </div>
   );
---
+++ client/src/pages/EditRestaurantPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function EditRestaurantPage() {
+  return <div>EditRestaurantPage</div>;
+}
+
+export default EditRestaurantPage;
