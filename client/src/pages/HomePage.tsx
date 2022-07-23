8f44b842a3377e0906a0bfa0c8fab1e42dbb2215
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-07-23 14:11:30 -0500

    Add: styles for homepage

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function HomePage() {
-  return <div>HomePage !!</div>;
+  return (
+    <div className='homePage container'>
+      <h1 className='mb-1'>All Restaurants</h1>
+      <section className='homePage__list section-md'>Holaa</section>
+    </div>
+  );
 }
 
 export default HomePage;
